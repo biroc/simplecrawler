55a9673b07d1902b0d3bb85db1b90d368dcc44f7
MironencoMircea <mircea.mironenco@gmail.com>
2016-05-23 15:21:56 +0300

    small fix

--- crawl.py
+++ crawl.py
@@ -1,10 +1,12 @@
 from Simple_Crawler import SimpleCrawler
 import sys
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Format: python3 crawl.py <domain>")
     else:
         domain = sys.argv[1]
         c = SimpleCrawler(domain=domain)
         c.start()
+        c.join()
+        print("Finished")
